a17ae35ce891f221fd4080e900040a02459b6504
MarijaNikolchova <marijanikolchova@hotmail.com>
2022-04-15 20:06:53 +0200

    Adding more videos for about.html

--- CreatingInteractivityWithCSSAndJavaScript/Forward-Fitness/scripts/script.js
+++ CreatingInteractivityWithCSSAndJavaScript/Forward-Fitness/scripts/script.js
@@ -7,6 +7,8 @@ Date: 14.04.2022
 var videoSource = document.getElementById("vid-src");
 var video = document.getElementById("example");
 var descriptionSource = document.getElementById("despsrc");
+video.style.width = "500px";
+video.style.height = "300px";
 
 // Hamburger menu function
 function hamburger() {
@@ -24,7 +26,23 @@ function hamburger() {
 // Function to display plank video
 function plank() {
     videoSource.src = "./media/plank.mp4";
-    // descriptionSource.src = "media/plankdescriptions.vtt"; 
+    // descriptionSource.src = "media/plank-descriptions.vtt"; 
+    video.style.display = "block";
+    video.load();
+}
+
+// Function to display burpees video
+function burpees() {
+    videoSource.src = "./media/burpees.mp4";
+    // descriptionSource.src = "media/burpees-descriptions.vtt"; 
+    video.style.display = "block";
+    video.load();
+}
+
+// Function to display Mountain climbing video
+function climbers() {
+    videoSource.src = "./media/climbers.mp4";
+    // descriptionSource.src = "media/climbers-descriptions.vtt"; 
     video.style.display = "block";
     video.load();
 }
