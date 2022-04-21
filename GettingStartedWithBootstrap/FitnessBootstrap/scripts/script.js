883166c24657703ba8a6f2b678565e89324e13f4
MarijaNikolchova <marijanikolchova@hotmail.com>
2022-04-21 20:59:03 +0200

    Creating a page with Bootstrap.

---
+++ GettingStartedWithBootstrap/FitnessBootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Marija Nikolchova
+    File Name: script.js 
+    Date: Today’s date
+*/
+
+//JQuery for hero image to consume the header window space
+$(document).ready(function () {
+    $(".hero").height($(window).height())
+});
